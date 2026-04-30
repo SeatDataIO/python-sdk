942540d012cd0bcc28c76a836781af6800155001
chaz <crisso@gmail.com>
2026-04-30 14:30:07 -0700

    feat: update __init__ exports for v1.0.0 (new exception names, types submodule)

--- seatdata/__init__.py
+++ seatdata/__init__.py
@@ -1,5 +1,13 @@
 from .client import SeatDataClient
 from .exceptions import (
+    SeatDataError,
+    SeatDataAuthError,
+    SeatDataRateLimitError,
+    SeatDataNotFoundError,
+    SeatDataInvalidRequestError,
+    SeatDataSubscriptionError,
+    SeatDataServerError,
+    CursorExpiredError,
     SeatDataException,
     AuthenticationError,
     RateLimitError,
@@ -7,14 +15,25 @@ from .exceptions import (
     NotFoundError,
     ServiceUnavailableError,
 )
+from . import types
 
-__version__ = "0.3.0"
+__version__ = "1.0.0"
 __all__ = [
     "SeatDataClient",
+    "SeatDataError",
+    "SeatDataAuthError",
+    "SeatDataRateLimitError",
+    "SeatDataNotFoundError",
+    "SeatDataInvalidRequestError",
+    "SeatDataSubscriptionError",
+    "SeatDataServerError",
+    "CursorExpiredError",
     "SeatDataException",
     "AuthenticationError",
     "RateLimitError",
     "SubscriptionError",
     "NotFoundError",
     "ServiceUnavailableError",
+    "types",
+    "__version__",
 ]
